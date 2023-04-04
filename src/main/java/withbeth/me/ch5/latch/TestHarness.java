159afce9b0f7a015ee8e6b260c7a7b9f1d00b16b
Bryan | JJ <with.beth@gmail.com>
2023-04-04 19:53:44 +0900

    feat(TestHarness): CountDownLatch이용한 모든스레드 동시시작부터 종료까지 걸리는 시간측정모듈 추가

---
+++ src/main/java/withbeth/me/ch5/latch/TestHarness.java
@@ -0,0 +1,54 @@
+package withbeth.me.ch5.latch;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestHarness {
+
+    public long measureExecutionTime(
+        final int nThreads,
+        final Runnable task
+    ) throws InterruptedException {
+        // Start latch as a binary latch
+        final CountDownLatch startLatch = new CountDownLatch(1);
+        final CountDownLatch endLatch = new CountDownLatch(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            Thread thread = createStartWaitingThread(startLatch, endLatch, task);
+            thread.start();
+        }
+
+        final long start = System.nanoTime();
+        startLatch.countDown();
+        endLatch.await();
+        final long end = System.nanoTime();
+        return end - start;
+    }
+
+    private Thread createStartWaitingThread(
+        final CountDownLatch startLatch,
+        final CountDownLatch endLatch,
+        final Runnable task
+    ) {
+        return new Thread(() -> {
+            try {
+                startLatch.await();
+                try {
+                    task.run();
+                } finally {
+                    endLatch.countDown();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        TestHarness testHarness = new TestHarness();
+        long executedTime = testHarness.measureExecutionTime(10, () ->
+            System.out.println("running task..."));
+        System.out.println(executedTime / Math.pow(10, 9));
+
+    }
+
+}
